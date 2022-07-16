5abba278073b5f6f16d24f43a7ef50a688fd8717
mr-zare <mo.erfan1379@gmail.com>
2022-07-16 21:39:34 +0430

    map level1_alpha

---
+++ Assets/Scripts/MapMove.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MapMove : MonoBehaviour
+{
+    [SerializeField] private Camera characterCamera;
+    [SerializeField] private Vector3 transformPos;
+    [SerializeField] private double factor;
+    [SerializeField] private GameObject map;
+    [SerializeField] private double speed;
+    [SerializeField] private int vec11;
+    [SerializeField] private int vec12;
+    [SerializeField] private int vec21;
+    [SerializeField] private int vec22;
+    [SerializeField] private bool isdo1;
+    [SerializeField] private bool isdo2;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (map.transform.position.z <vec11 && map.transform.position.z >vec12 && isdo1==false)
+        {
+            isdo1 = true;
+            speed += factor;
+        }
+        if (map.transform.position.z <vec21 && map.transform.position.z >vec22 && isdo2==false)
+        {
+            isdo2 = true;
+            speed += factor;
+        }
+        transformPos = map.transform.position;
+        transformPos.z =(float) (transformPos.z - speed);
+        map.transform.position = transformPos;
+        
+    }
+}
